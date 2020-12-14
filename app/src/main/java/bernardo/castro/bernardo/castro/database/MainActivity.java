8774c231aa74b2501bd38c223ef83efbb9197677
Bernardo Castro <bernardoc544@gmail.com>
2020-12-14 15:39:19 -0500

    First Commit

---
+++ app/src/main/java/bernardo/castro/bernardo/castro/database/MainActivity.java
@@ -0,0 +1,31 @@
+package bernardo.castro.bernardo.castro.database;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b = findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("message");
+
+                myRef.setValue("Hello, World!");
+                Toast.makeText(getApplicationContext(), "Firebase Working", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
