63bc130e803b59f3a0efa9fb41da7780c534b80d
Bernardo Castro <bernardoc544@gmail.com>
2021-01-06 15:04:19 -0500

    Populate Spinner and Patient Model

--- app/src/main/java/bernardo/castro/bernardo/castro/database/PatientActivity.java
+++ app/src/main/java/bernardo/castro/bernardo/castro/database/PatientActivity.java
@@ -3,6 +3,9 @@ package bernardo.castro.bernardo.castro.database;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class PatientActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class PatientActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient);
+        EditText name = findViewById(R.id.bernardoPatientName);
+        EditText lastName = findViewById(R.id.bernardoPatientLastName);
+        Spinner department = findViewById(R.id.bernardoDepartments);
+
+        ArrayAdapter<CharSequence> departments = ArrayAdapter.createFromResource(this, R.array.departments,
+                android.R.layout.simple_spinner_item);
+        departments.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        department.setAdapter(departments);
+
+        
     }
 }
